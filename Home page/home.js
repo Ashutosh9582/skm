2db00a4a8ff19171012e3ae8a31c9954e7fca5ef
Ashutosh9582 <ashukhandal0001@gmail.com>
2024-12-28 23:03:07 +0530

    nothing is changed as i started its

---
+++ Home page/home.js
@@ -0,0 +1,16 @@
+console.log('first program');
+{
+    let x=5;
+    let y=10;
+    let z;
+    z=x+y;
+    console.log(z);
+}
+
+{
+    let x=5;
+    let y=10;
+    let z=55;
+    a=x+y+z;
+    console.log(a);
+}
